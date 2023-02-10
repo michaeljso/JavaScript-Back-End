c33b454be76ac723bb4c2788263a0993a695e758
Michael Jonatas <michael_jso@hotmail.com>
2023-02-10 09:41:59 -0300

    Erros

---
+++ JavaScript e NodeJS/console.js
@@ -0,0 +1,10 @@
+//log -> registro
+const minhaVar = true;
+
+console.log(245);
+console.log("Eu sou um texto");
+console.log(minhaVar);
+
+//console.error() - coloca mensagem de erro  - tratamento de erro
+
+console.error('Erro!');
