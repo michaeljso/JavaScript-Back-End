ef4a2f0c64dd2af6fb336c31b11629b1e1faf965
Michael Jonatas <michael_jso@hotmail.com>
2023-02-25 10:34:00 -0300

    Removendo valores e adiconando novos

---
+++ JavaScript - Arrays/atualizando-lista.js
@@ -0,0 +1,9 @@
+const nomes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+
+// (qual indice quer remover, quantos indices remover depois do primeiro, adicona novo valor ao array)
+nomes.splice(1, 2, 'Rodrigo');
+
+//adicionando aluno com push
+//nomes.push('Rodrigo');
+
+console.log(nomes);
