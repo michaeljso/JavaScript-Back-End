780d5f9bc96510ac31bdf263cf30724101b5c682
Michael Jonatas <michael_jso@hotmail.com>
2023-02-27 09:00:08 -0300

    Juntando Arrays

---
+++ JavaScript - Arrays/juntando-salas.js
@@ -0,0 +1,7 @@
+const salaJS = ['Evaldo', 'Camis','Mari'];
+const salaPython = ['Ju', 'Leo', 'Raquel'];
+
+// o .concat junta dois ou mais arrais retornando em um novo array
+const salaUnificada = salaJS.concat(salaPython);
+
+console.log(salaUnificada);
