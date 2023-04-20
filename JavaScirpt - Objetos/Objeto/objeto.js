dc66b8a10d544fac5b390416abbb307d094bbd9d
Michael Jonatas <michael_jso@hotmail.com>
2023-04-20 08:59:46 -0300

    Objeto

---
+++ JavaScirpt - Objetos/Objeto/objeto.js
@@ -0,0 +1,18 @@
+
+const listaCPFs = [111111,222222,33333];
+const informacoesPessoa = ["nome","Jose","Idade",32,"CPF","112222333"];
+
+//console.log(informacoesPessoa[1]);
+
+
+//objeto 
+
+//chave: valor,
+const objetoPessoa = {
+    nome: "Jose",
+    idade:32,
+    cpf: "1112223333" 
+}
+
+
+console.log(objetoPessoa.idade);
