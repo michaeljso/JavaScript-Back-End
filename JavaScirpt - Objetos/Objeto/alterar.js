f381bb9135cf37bddae9398537da3e6ff55155ba
Michael Jonatas <michael_jso@hotmail.com>
2024-02-26 08:56:21 -0300

    Alterando Objeto

--- JavaScirpt - Objetos/Objeto/alterar.js
+++ JavaScirpt - Objetos/Objeto/alterar.js
@@ -3,4 +3,27 @@ const pessoa = {
     profissao: "Dev",
 }
 
-console.log(nome.pessoa);
+console.log(pessoa.nome);
+
+//como não foi declarada retorna undefined
+console.log(pessoa.telefone);
+
+//definindo depois de criado
+pessoa.telefone = "62996806162";
+
+console.log(pessoa.telefone);
+
+//adiconando sobrenome
+//substitui o nome
+pessoa.nome = "Jonatas da Silva";
+
+console.log(pessoa);
+
+
+//Erro
+const novaPessoa = {
+    nome: "Pedro",
+};
+
+pessoa = novaPessoa;
+
