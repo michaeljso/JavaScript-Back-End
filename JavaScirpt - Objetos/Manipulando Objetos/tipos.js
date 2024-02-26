88a497e6efc5a499eef35ba423f50c7caa9afe62
Michael Jonatas <michael_jso@hotmail.com>
2024-02-26 09:04:00 -0300

    Manipulando Objeto

---
+++ JavaScirpt - Objetos/Manipulando Objetos/tipos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555500","14444444000"],//lista
+};
+
+console.log(cliente);
+
+console.log(cliente.telefone);
