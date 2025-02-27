c1158c99a5d7e9f030e58f75864a893b3a46c45b
Michael Jonatas <michael_jso@hotmail.com>
2025-02-27 14:54:04 -0300

    Objetos e listas dentro de um objeto

---
+++ JavaScirpt - Objetos/Objeto/objs-aninhados.js
@@ -0,0 +1,22 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '05640181125',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['62994280497','62996806162'] //array  -- dados de um mesmo formato
+}
+
+//adicionando obj dentro do obj -- dados de um mesmo contexto
+estudante.endereco = {
+    rua: 'Rua Benedito Luiz dias',
+    numero: '45',
+    complemento: 'Apto 43'
+}
+
+console.log(estudante);
+
+console.log(estudante.endereco);
+
+//pegar apenas a rua (obj dentro do obj)
+console.log(estudante.endereco.rua);
