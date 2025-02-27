839c08129943bcf1a82cea707b5ad5be2bdd0577
Michael Jonatas <michael_jso@hotmail.com>
2025-02-27 14:45:30 -0300

    Dados e valores

---
+++ JavaScirpt - Objetos/Objeto/dados-valores.js
@@ -0,0 +1,12 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '05640181125',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['62994280497','62996806162'] //array
+}
+
+console.log(estudante.telefones);
+
+console.log(estudante.telefones[0]); //acessa somente o primeiro dado do array
